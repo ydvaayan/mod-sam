cb02ca3ca45aaee262277720b708aeb851a0d8e2
Aayan Yadav <ydvaayan@gmail.com>
2023-11-07 02:52:42 +0530

    implement argparse in embedding.py

--- demo/embedding.py
+++ demo/embedding.py
@@ -9,13 +9,32 @@ import onnxruntime
 from onnxruntime.quantization import QuantType
 from onnxruntime.quantization.quantize import quantize_dynamic
 
-checkpoint = "../sam_vit_h_4b8939.pth"
-model_type = "vit_h"
-sam = sam_model_registry[model_type](checkpoint=checkpoint)
-sam.to(device='cpu')
-predictor = SamPredictor(sam)
+parser = argparse.ArgumentParser(
+    description="Convert demo output to RLE format"
+)
+parser.add_argument(
+    "--input_path", type=str, required=True, help="The path to the input image."
+)
+parser.add_argument(
+    "--output_path", type=str, required=True, help="The path to the output embedding."
+)
+parser.add_argument(
+    "--checkpoint", type=str, default="../sam_vit_h_4b8939.pth", help="id of image whose mask is being changed"
+)
+parser.add_argument(
+    "--model_type", type=str, default= "vit_h", help="id of mask that is being changed."
+)
 
-image = cv2.imread('src/assets/data/000000073118.jpg')
-predictor.set_image(image)
-image_embedding = predictor.get_image_embedding().cpu().numpy()
-np.save("src/assets/data/000000073118.npy", image_embedding)
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    checkpoint = args.checkpoint
+    model_type = args.model_type
+    sam = sam_model_registry[model_type](checkpoint=checkpoint)
+    sam.to(device='cpu')
+    predictor = SamPredictor(sam)
+
+    image = cv2.imread(args.input_path)
+    predictor.set_image(image)
+    image_embedding = predictor.get_image_embedding().cpu().numpy()
+    np.save(args.output_path, image_embedding)
